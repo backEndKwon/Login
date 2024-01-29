67cb9be05a76c00197c04b55e2ab825897d57f1e
backEndKwon <rhaehfdl0433@gmail.com>
2024-01-29 21:22:20 +0900

    feat: swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//utils/swagger에서 swagger관련 setting후 main에 등록
+import { setUpSwagger } from './utils/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setUpSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
