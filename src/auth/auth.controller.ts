591d2a752b2cad98bcf5cff661e61deb7427b0c1
backEndKwon <rhaehfdl0433@gmail.com>
2024-01-30 14:06:23 +0900

    feat: swagger 기본 세팅 확인 완료

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
+//swagger적용하기 위해서는 Api태그, 오퍼레이션, 리스폰스 필요
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
+//tag = 큰 제목
+@ApiTags('AUTH API CHECK')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-
-    // api 확인용
-    @Get('home')
-    check() {
-        return this.authService.check(); // Assuming you have a method named 'check' in your AuthService
-    }
-    
-
+  // api 확인용
+  @Get('home')
+  //operation = 동작
+  @ApiOperation({
+    summary: '송수신 확인용 API',
+    description: 'GET방식을 보내 반환값을 잘 return하는지 확인',
+  })
+  //response = 반환값
+  @ApiResponse({ description: 'connected success를 반환한다.' })
+  check() {
+    return this.authService.check();
+  }
 }
